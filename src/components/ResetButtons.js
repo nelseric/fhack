630f22a736a44ce23b0b3d9a6f69ca0018177e41
Eric Nelson <enelson@eligoenergy.com>
2015-12-07 15:03:03 -0600

    Added reset all button

--- src/components/ResetButtons.js
+++ src/components/ResetButtons.js
@@ -1,15 +1,23 @@
 import React, {Component} from 'react';
 
 class ResetButtons extends Component {
+  _resetAll() {
+    this.props.resetConstraints();
+    this.props.resetWords();
+  }
   render() {
     return (
-      <button onClick={this.props.resetConstraints}>Reset Constraints</button>
+      <div className="ResetButtons">
+        <button onClick={this.props.resetConstraints}>Reset Constraints</button>
+        <button onClick={this._resetAll.bind(this)}>Reset All</button>
+      </div>
     );
   }
 }
 
 ResetButtons.propTypes = {
   resetConstraints: React.PropTypes.func.isRequired,
+  resetWords: React.PropTypes.func.isRequired,
 };
 
 export default ResetButtons;
